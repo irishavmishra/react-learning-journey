074e1d04ab07b41557ed7a401037b1acf1abe134
irishavmishra <irishavmishra@proton.me>
2024-01-01 23:40:12 +0530

    Intro to props and children

--- UsingReactViaCDN/src/index.js
+++ UsingReactViaCDN/src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const child = React.createElement("p", {}, "This is child");
-const div = React.createElement(
-  "div",
-  { className: "text" },
-  //   "Hello from rishav, i am inside react now"
-  child
-);
-
-// const div = <div className="text">Hello 2</div>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(div);
+root.render(<App />);
